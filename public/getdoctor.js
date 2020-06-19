91b0d7cb8ed8e7ddb36452147c6c8808afae4b4a
SARAN M <31407429+SARAN-M@users.noreply.github.com>
2020-06-19 12:35:04 +0530

    Update getdoctor.js

--- public/getdoctor.js
+++ public/getdoctor.js
@@ -5,26 +5,27 @@ $("#symptoms").change(function(){
        return data
       });
       var list=[
-        {name:"Dr.AK",symtoms:"lungs"},
-        {name:"Dr.NK",symtoms:"eye"},
-        {name:"Dr.PK",symtoms:"heart"},
-        {name:"Dr.MK",symtoms:"sugar"},
-        {name:"Dr.CK",symtoms:"ear"}
+        {name:"Dr.AK",symtoms:"lungs",time:"3.00 AM"},
+        {name:"Dr.NK",symtoms:"eye",time:"6.00 PM"},
+        {name:"Dr.PK",symtoms:"heart",time:"10.00 AM"},
+        {name:"Dr.MK",symtoms:"sugar",time:"9.00 PM"},
+        {name:"Dr.CK",symtoms:"ear",time:"12.00 PM"}
     ]
     const n=list.filter(function(data){
         if(data.symtoms == select){
             return data.name;
+            return data.time;
         }
         else{
             return null;
         }
     });
-    console.log(n);
     n.forEach(function(list){
         var name=$('#drname').val(list.name);
+        var name=$('#time').val(list.time);
     })
-    
-   
+    console.log(n);
+
+})
       
     
-})
